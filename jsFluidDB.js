355e3c90ff78489435d06f44640c17af577d0f31
Emanuel Carnevale <emanuel.carnevale@gmail.com>
2009-10-01 19:31:18 +0200

    added base64 encoding, added PUT and DELETE, added basic authentication

--- jsFluidDB.js
+++ jsFluidDB.js
@@ -15,16 +15,25 @@ fluidDB.choose = function(type){
 
 fluidDB.choose('main');
 
-fluidDB.ajax = function(type, url, payload, callback, async_req){
+fluidDB.ajax = function(type, url, payload, callback, async_req, username, password){
+    if((username != undefined) && (password != undefined)){
+      var authenticate = true;
+    }
+    
     if(async_req == undefined){
       async_req = true;
     }
     $.ajax({
           async: async_req,
           beforeSend: function(xhrObj){
-              if(fluidDB.baseURL == fluidDB.instance['main']){
+              if(authenticate){
+                  var base64string = username + ":" + password;
+                  xhrObj.setRequestHeader("Authorization","Basic "+  Base64.encode(base64string));
+              };
+              if(fluidDB.baseURL == fluidDB.instance.main){
                   xhrObj.setRequestHeader("Accept","application/json");
               };
+             
               xhrObj.setRequestHeader("Content-Type","application/json");
           },
           contentType: "application/json",
@@ -36,13 +45,165 @@ fluidDB.ajax = function(type, url, payload, callback, async_req){
     });
 }
 
-fluidDB.get = function(url, callback, async_req){
-    fluidDB.ajax("GET", fluidDB.baseURL+url, null, callback, async_req);
+
+fluidDB.get = function(url, callback, async_req, username, password){
+    fluidDB.ajax("GET", fluidDB.baseURL+url, null, callback, async_req, username, password);
 }
 
-fluidDB.post = function(url, payload, callback, async_req){
-    fluidDB.ajax("POST", fluidDB.baseURL+url, payload, callback, async_req);
+fluidDB.post = function(url, payload, callback, async_req, username, password){
+    fluidDB.ajax("POST", fluidDB.baseURL+url, payload, callback, async_req, username, password);
+}
+
+fluidDB.put = function(url, payload, callback, async_req, username, password){
+    fluidDB.ajax("PUT", fluidDB.baseURL+url, payload, callback, async_req, username, password);
+}
+
+fluidDB.delete = function(url, callback, async_req, username, password){
+    fluidDB.ajax("DELETE", fluidDB.baseURL+url, null, callback, async_req, username, password);
 }
 
 
 //END FluidDB REST LIB
+
+//BEGIN Base64 LIB
+/* Taken from http://www.webtoolkit.info/javascript-base64.html
+*  eventually is going to be moved outside this file 
+*/
+
+var Base64 = {
+ 
+	// private property
+	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+ 
+	// public method for encoding
+	encode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = Base64._utf8_encode(input);
+ 
+		while (i < input.length) {
+ 
+			chr1 = input.charCodeAt(i++);
+			chr2 = input.charCodeAt(i++);
+			chr3 = input.charCodeAt(i++);
+ 
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
+ 
+			if (isNaN(chr2)) {
+				enc3 = enc4 = 64;
+			} else if (isNaN(chr3)) {
+				enc4 = 64;
+			}
+ 
+			output = output +
+			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+ 
+		}
+ 
+		return output;
+	},
+ 
+	// public method for decoding
+	decode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+ 
+		while (i < input.length) {
+ 
+			enc1 = this._keyStr.indexOf(input.charAt(i++));
+			enc2 = this._keyStr.indexOf(input.charAt(i++));
+			enc3 = this._keyStr.indexOf(input.charAt(i++));
+			enc4 = this._keyStr.indexOf(input.charAt(i++));
+ 
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+ 
+			output = output + String.fromCharCode(chr1);
+ 
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+ 
+		}
+ 
+		output = Base64._utf8_decode(output);
+ 
+		return output;
+ 
+	},
+ 
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+ 
+		for (var n = 0; n < string.length; n++) {
+ 
+			var c = string.charCodeAt(n);
+ 
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+ 
+		}
+ 
+		return utftext;
+	},
+ 
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+ 
+		while ( i < utftext.length ) {
+ 
+			c = utftext.charCodeAt(i);
+ 
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+ 
+		}
+ 
+		return string;
+	}
+ 
+}
+
+//END Base64 LIB
