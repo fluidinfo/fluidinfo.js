11214004f66066c247b8e9073d15e4c661e60088
Nicholas H.Tollervey <ntoll@ntoll.org>
2011-07-29 20:35:32 +0100

    Removed dependency on jQuery from the library. Hid the utility functions and the call that handles the XHR request to Fluidinfo so the fluidinfo object's interface is very simple and leaves no room for overriding how the request is sent/handled by the library.

--- fluidinfo.js
+++ fluidinfo.js
@@ -4,7 +4,17 @@
  * @author <a href="http://twitter.com/onigiri">onigiri</a>, <a href="http://twitter.com/ntoll">ntoll</a> & <a href="http://twitter.com/barshirtcliff">barshirtcliff</a>
  * @version 0.1
  * @constructor
- * @param options {Object} Contains various parameters for the call.
+ * @param options {Object} Contains various parameters for the new session with
+ * Fluidinfo.
+ * <dl>
+ *   <dt>username</dt>
+ *   <dd>The username to use when authenticating with Fluidinfo.</dd>
+ *   <dt>password</dt>
+ *   <dd>The password to use when authenticating with Fluidinfo</dd>
+ *   <dt>instance</dt>
+ *   <dd>The instance to connect to. Either "main", "sandbox" or a bespoke
+ *  instance. Defaults to "main".</dd>
+ * </dl>
  * returns {Object} An object through which one interacts with Fluidinfo.
  */
 fluidinfo = function(options) {
@@ -63,8 +73,10 @@ fluidinfo = function(options) {
         }
     }
 
-    // The object to eventually be returned
-    session = new Object();
+    /**
+     * Represents a session with Fluidinfo.
+     */
+    var session = new Object();
 
     if(options) {
       if(options.instance) {
@@ -88,7 +100,6 @@ fluidinfo = function(options) {
               }
         }
       }
-
       if((options.username != undefined) && (options.password != undefined)) {
         session.authorizationToken = Base64.encode(options.username + ":" + options.password);
       }
@@ -99,8 +110,6 @@ fluidinfo = function(options) {
       session.baseURL = "https://fluiddb.fluidinfo.com/";
     }
 
-    var utils = new Object();
-
     /**
      * Magic voodoo used to identify an array (potentially a Fluidinfo set).
      * Taken from page 61 of Doug Crockford's "Javascript: The Good Parts".
@@ -108,7 +117,7 @@ fluidinfo = function(options) {
      * @param value A value that might be an array.
      * @return {boolean} An indication if the value is an array.
      */
-    utils.isArray = function(value) {
+    function isArray(value) {
       return Object.prototype.toString.apply(value) === "[object Array]";
     };
 
@@ -120,7 +129,7 @@ fluidinfo = function(options) {
      * '/' values.
      * @return {string} The appropriately encoded URL.
      */
-    utils.encodeURL = function(path) {
+    function encodeURL(path) {
       var result = "";
       for(i=0; i<path.length; i++) {
         result += "/" + encodeURIComponent(path[i]);
@@ -137,7 +146,7 @@ fluidinfo = function(options) {
      * @param value A value whose "type" needs identifying
      * @return A boolean indication if the value is a Fluidinfo primitive type.
      */
-    utils.isPrimitive = function(value) {
+    function isPrimitive(value) {
       // check the easy type matches first
       var valueType = typeof(value);
       var primitiveTypes = ["number", "string", "boolean"];
@@ -152,7 +161,7 @@ fluidinfo = function(options) {
       }
       // check for an array (potential set) and validate it only contains
       // strings (currently multi-type arrays are not allowed)
-      if(utils.isArray(value)) {
+      if(isArray(value)) {
         for(i=0; i<value.length; i++) {
           memberType = typeof(value[i]);
           if(memberType !== "string") {
@@ -174,7 +183,7 @@ fluidinfo = function(options) {
      * @return the most appropriate MIME to use of null if not appropriate /
      * required.
      */
-    utils.detectContentType = function(options) {
+    function detectContentType(options) {
       // a "PUT" to objects/ or about/ endpoints means dealing with the MIME
       // of a tag-value. If no MIME type is passed in the options objects
       // then check if the value is a Fluidinfo primitive type. Otherwise
@@ -183,7 +192,7 @@ fluidinfo = function(options) {
       if(options.type==="PUT" && (options.url.match("^objects\/") || options.url.match("^about\/"))) {
         if(options.contentType){
           result = options.contentType;
-        } else if (api.utils.isPrimitive(options.data)) {
+        } else if (isPrimitive(options.data)) {
           result = "application/vnd.fluiddb.value+json";
         } else {
           throw { name: "ValueError", message: "Must supply Content-Type"};
@@ -195,34 +204,88 @@ fluidinfo = function(options) {
       return result;
     }
 
-    var api = new Object();
-
-    api.utils = utils;
+    /**
+     * Returns an appropriate XMLHttpRequest Object depending on the browser.
+     * Based upon code from here:
+     * http://www.quirksmode.org/js/xmlhttp.html
+     */
+    function createXMLHTTPObject() {
+      var XMLHttpFactories = [
+        function () {return new XMLHttpRequest()},
+        function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+        function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+        function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+      ];
+      var xhr = false;
+      for(var i=0; i<XMLHttpFactories.length; i++) {
+        try {
+          xhr = XMLHttpFactories[i]();
+        } catch(e) {
+          continue;
+        }
+        break;
+      }
+      return xhr;
+    }
 
     /**
-     * Makes an appropriate AJAX request to Fluidinfo
+     * Sends an appropriate XMLHTTPRequest based request to Fluidinfo.
+     * @param options {Object} An object containing the following named options:
+     * <dl>
+     *  <dt>type</dt>
+     *  <dd>The request's HTTP method. [GET, POST, PUT, DELETE or HEAD]</dd>
+     *  <dt>url</dt>
+     *  <dd>The full URL of the resource to which the request is made.</dd>
+     *  <dt>data</dt>
+     *  <dd>The body of data that may be the payload of the request (in the
+     *  case of POST and PUT requests).</dd>
+     *  <dt>async</dt>
+     *  <dd>Indicates if the request is to be asyncronous (default is true)</dd>
+     *  <dt>onSuccess</dt>
+     *  <dd>A function that takes the XHR request as an argument. Called upon
+     *  successful completion of the request.</dd>
+     *  <dt>onError</dt>
+     *  <dd>A function that takes the XHR request as an argument. Called when
+     *  the request resulted in an error.</dd>
+     * </dl>
      *
-     * @private
-     * @param options {Object} Contains various parameters for the call.
      */
-    api.ajax = function(options){
-      options.contentType = api.utils.detectContentType(options);
-      if(api.utils.isArray(options.url)) {
-        options.url = api.utils.encodeURL(options.url);
+    function sendRequest(options) {
+      if(isArray(options.url)) {
+        options.url = encodeURL(options.url);
       }
-      options.url = session.baseURL+options.url;
-      options.async = options.async || true;
-      options.beforeSend = function(xhrObj){
-        if(session.authorizationToken != undefined){
-          xhrObj.setRequestHeader("Authorization","Basic " + session.authorizationToken);
-        };
-        if(options.contentType) {
-          xhrObj.setRequestHeader("Content-Type", options.contentType);
-        }
+      var method = options.type.toUpperCase() || "GET";
+      var url = session.baseURL+options.url;
+      var async = options.async || true;
+      var xhr = createXMLHTTPObject();
+      if(!xhr) return;
+      xhr.open(method, url, async);
+      if(session.authorizationToken != undefined){
+        xhr.setRequestHeader("Authorization", "Basic " + session.authorizationToken);
       };
-      options.processData = false;
-      $.ajax(options);
+      var contentType = detectContentType(options);
+      if(contentType) {
+        xhr.setRequestHeader("Content-Type", contentType);
+      }
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState != 4) return;
+        if(xhr.status < 300 && xhr.status != 304) {
+          // there appears to be a problem
+          if(options.onError){
+            options.onError(xhr);
+          }
+        } else if (options.onSuccess){
+          options.onSuccess(xhr);
+        }
+      }
+      if(xhr.readyState == 4) return xhr;
+      xhr.send(options.data)
     }
+
+    /**
+     * Contains functions to facilitate the calling of the Fluidinfo REST API.
+     */
+    var api = new Object();
 
     /**
      * Makes an HTTP GET call to the Fluidinfo API
@@ -231,7 +294,7 @@ fluidinfo = function(options) {
     api.get = function(options){
       options.type = "GET";
       options.data = null;
-      session.api.ajax(options);
+      sendRequest(options);
     }
 
     /**
@@ -240,7 +303,7 @@ fluidinfo = function(options) {
      */
     api.post = function(options){
       options.type = "POST";
-      session.api.ajax(options);
+      sendRequest(options);
     }
 
     /**
@@ -249,7 +312,7 @@ fluidinfo = function(options) {
      */
     api.put = function(options){
       options.type = "PUT";
-      session.api.ajax(options);
+      sendRequest(options);
     }
 
     /**
@@ -259,7 +322,7 @@ fluidinfo = function(options) {
     api.delete = function(options){
       options.type = "DELETE";
       options.data = null;
-      session.api.ajax(options);
+      sendRequest(options);
     }
 
     /**
@@ -269,7 +332,7 @@ fluidinfo = function(options) {
     api.head = function(options){
       options.type = "HEAD";
       options.data = null;
-      session.api.ajax(options);
+      sendRequest(options);
     }
 
     session.api = api;
--- spec/fluidinfoSpec.js
+++ spec/fluidinfoSpec.js
@@ -58,10 +58,11 @@ function it_should_have_a_content_type_of(type) {
 describe("Fluidinfo.js", function() {
 
   beforeEach(function() {
+    this.server = sinon.fakeServer.create();
+    this.xhr = sinon.useFakeXMLHttpRequest();
     fi = fluidinfo({ username: "username",
                      password: "password"
                    });
-    this.server = sinon.fakeServer.create();
   });
 
   /**
@@ -69,7 +70,7 @@ describe("Fluidinfo.js", function() {
    */
   describe("Configuration", function() {
 
-    it("as default it should point to the main instance", function() {
+    it("should default to point to the main instance", function() {
       expect(fi.baseURL).toEqual("https://fluiddb.fluidinfo.com/");
       expect(fi.authorizationToken).not.toEqual(undefined);
     });
@@ -135,14 +136,12 @@ describe("Fluidinfo.js", function() {
    */
   describe("API", function() {
 
-    describe("ajax", function() {
+    describe("Request configuration", function() {
       it("should correctly set content-type on a primitive value PUT to the /objects endpoint", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "objects/fakeObjectID/username/tag";
         options.data = 1.234;
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.put(options);
         expected = "application/vnd.fluiddb.value+json";
         actual = this.server.requests[0].requestHeaders['Content-Type'];
         expect(actual).toContain(expected);
@@ -150,11 +149,9 @@ describe("Fluidinfo.js", function() {
 
       it("should correctly set content-type on a primitive value PUT to the /about endpoint", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.data = 1.234;
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.put(options);
         expected = "application/vnd.fluiddb.value+json";
         actual = this.server.requests[0].requestHeaders['Content-Type'];
         expect(actual).toContain(expected);
@@ -162,12 +159,10 @@ describe("Fluidinfo.js", function() {
 
       it("should correctly set content-type on a passed in MIME as PUT to the /objects endpoint", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "objects/fakeObjectID/username/tag";
         options.data = "<html><body><h1>Hello, world!</h1></body></html>";
         options.contentType = "text/html";
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.put(options);
         expected = "text/html";
         actual = this.server.requests[0].requestHeaders['Content-Type'];
         expect(actual).toContain(expected);
@@ -175,12 +170,10 @@ describe("Fluidinfo.js", function() {
 
       it("should correctly set content-type on a passed in MIME as PUT to the /about endpoint", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.data = "<html><body><h1>Hello, world!</h1></body></html>";
         options.contentType = "text/html";
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.put(options);
         expected = "text/html";
         actual = this.server.requests[0].requestHeaders['Content-Type'];
         expect(actual).toContain(expected);
@@ -188,12 +181,10 @@ describe("Fluidinfo.js", function() {
 
       it("should complain if it can't set a content-type on a PUT to /objects", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "objects/fakeObjectID/username/tag";
         options.data = {"foo": "bar"};
-        options.success = function(data){};
         try {
-          fi.api.ajax(options);
+          fi.api.put(options);
         } catch(e) {
           var exception = e;
         }
@@ -202,12 +193,10 @@ describe("Fluidinfo.js", function() {
 
       it("should complain if it can't set a content-type on a PUT to /about", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.data = {"foo": "bar"};
-        options.success = function(data){};
         try {
-          fi.api.ajax(options);
+          fi.api.put(options);
         } catch(e) {
           var exception = e;
         }
@@ -216,11 +205,9 @@ describe("Fluidinfo.js", function() {
 
       it("should set the content-type to to 'application/json' by default", function() {
         var options = new Object();
-        options.type = "POST";
         options.url = "namespaces/test";
         options.data = {name: "foo", description: "bar"};
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.post(options);
         expected = "application/json";
         actual = this.server.requests[0].requestHeaders['Content-Type'];
         expect(actual).toContain(expected);
@@ -228,11 +215,9 @@ describe("Fluidinfo.js", function() {
 
       it("should appropriately encode a URL passed as an array", function() {
         var options = new Object();
-        options.type = "POST";
         options.url = ["about", "än/- object", "namespace", "tag"];
         options.data = {name: "foo", description: "bar"};
-        options.success = function(data){};
-        fi.api.ajax(options);
+        fi.api.post(options);
         expect(this.server.requests[0].url)
           .toEqual(fi.baseURL+"about/%C3%A4n%2F-%20object/namespace/tag");
       })
@@ -373,61 +358,38 @@ describe("Fluidinfo.js", function() {
   describe("API utilities", function(){
 
     /**
-     * Unfortunately using the typeof function doesn't work when trying to
-     * identify an Array. This tests the utility function we use for the job.
-     */
-    describe("Array (list/set) matching", function() {
-      it("should identify an array", function() {
-        expect(fi.api.utils.isArray([1, 2, 3])).toEqual(true);
-      });
-
-      it("should not identify an object as an array", function() {
-        expect(fi.api.utils.isArray({"foo": "bar"})).toEqual(false);
-      });
-    });
-
-    /**
-     * Ensures the library correctly encodes URLs that are passed in as either
-     * an array of values to escape or a single string to escape.
-     */
-    describe("URL escaping", function() {
-      it("should correctly encode a URL path passed as an array", function() {
-        var path = ["about", "än/- object", "namespace", "tag"];
-        var expected = "about/%C3%A4n%2F-%20object/namespace/tag";
-        expect(fi.api.utils.encodeURL(path)).toEqual(expected);
-      });
-    });
-
-    /**
      * Checks the library correctly detects the appropriate MIME type to set for
      * the eventual value of the Content-Type header of a request.
      */
     describe("Content-Type detection", function() {
       it("should identify a primitive in a PUT to 'objects'", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "objects/fakeObjectID/username/tag";
         options.data = 1.234;
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual("application/vnd.fluiddb.value+json");
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a primitive in a PUT to 'about'", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.data = 1.234;
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual("application/vnd.fluiddb.value+json");
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a given MIME in a PUT to 'objects'", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "objects/fakeObjectID/username/tag";
         options.contentType = "text/html";
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual("text/html");
+        fi.api.put(options);
+        expected = "text/html";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a given MIME in a PUT to 'about'", function() {
@@ -435,38 +397,32 @@ describe("Fluidinfo.js", function() {
         options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.contentType = "text/html";
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual("text/html");
+        fi.api.put(options);
+        expected = "text/html";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should default to JSON for all other requests with data", function() {
         var options = new Object();
-        options.type = "POST";
         options.url = "namespaces/test";
         options.data = {name: "foo", description: "bar"};
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual("application/json");
+        fi.api.post(options);
+        expected = "application/json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should complain if it can't detect the MIME", function() {
         var options = new Object();
-        options.type = "PUT";
         options.url = "about/fakeAboutValue/username/tag";
         options.data = new Object();
         try {
-          fi.api.utils.detectContentType(options);
+          fi.api.put(options);
         } catch(e) {
           var exception = e;
         }
         expect(exception.name).toEqual("ValueError");
-      });
-
-      it("should return null if no data is being sent", function() {
-        var options = new Object();
-        options.type = "GET";
-        options.url = "namespaces/test";
-        expect(fi.api.utils.detectContentType(options))
-          .toEqual(null);
       });
     });
 
@@ -475,39 +431,87 @@ describe("Fluidinfo.js", function() {
      */
     describe("Primitive identification", function() {
       it("should identify an integer as primitive", function() {
-        expect(fi.api.utils.isPrimitive(1)).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = 1;
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a float as primitive", function() {
-        expect(fi.api.utils.isPrimitive(1.1)).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = 1.234;
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a boolean as primitive", function() {
-        expect(fi.api.utils.isPrimitive(false)).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = false;
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a string as primitive", function() {
-        expect(fi.api.utils.isPrimitive("hello")).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = "hello";
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a null as primitive", function() {
-        expect(fi.api.utils.isPrimitive(null)).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = null;
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a string array as primitive", function() {
-        expect(fi.api.utils.isPrimitive(["a", "b", "c"])).toEqual(true);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = ["a", "b", "c"];
+        fi.api.put(options);
+        expected = "application/vnd.fluiddb.value+json";
+        actual = this.server.requests[0].requestHeaders['Content-Type'];
+        expect(actual).toContain(expected);
       });
 
       it("should identify a mixed array as NOT primitive", function() {
-        expect(fi.api.utils.isPrimitive(["a", "b", 1])).toEqual(false);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = ["a", "b", 1];
+        try {
+          fi.api.put(options);
+        } catch(e) {
+          var exception = e;
+        }
+        expect(exception.name).toEqual("ValueError");
       });
 
       it("should identify an object as NOT primitive", function() {
-        expect(fi.api.utils.isPrimitive(["a", "b", 1])).toEqual(false);
-      });
-
-      it("should identify a function as NOT primitive", function() {
-        expect(fi.api.utils.isPrimitive(function(){})).toEqual(false);
+        var options = new Object();
+        options.url = "about/fakeAboutValue/username/tag";
+        options.data = {foo: "bar"};
+        try {
+          fi.api.put(options);
+        } catch(e) {
+          var exception = e;
+        }
+        expect(exception.name).toEqual("ValueError");
       });
     });
   });
